f544205940e793a2f23848356dedc1e013d9a715
Alex Jousse <alexandre.jousse@amadeus.com>
2020-04-25 16:27:54 -0400

    Start beautifying and add play and cue possibility

--- static/js/youtube.js
+++ static/js/youtube.js
@@ -5,6 +5,8 @@ var output = document.getElementById("output");
 var hostname = window.location.hostname
 var port = window.location.port
 var websocket_protocol = "wss"
+var nextVideos = []
+
 
 // Handle local dev cases
 if (port.includes('80')) {
@@ -18,19 +20,20 @@ socket.onopen = function () {
 };
 
 socket.onmessage = function (e) {
-    if (e.data.includes('videoId')) {
+    if (e.data.includes('playVideoId')) {
         obj = JSON.parse(e.data)
         
-        document.getElementById('output').innerHTML += "This is the json with video id: " + obj.videoId + " \n";
-        if (obj.videoId == youtubeVideoId) {
+        document.getElementById('output').innerHTML += "This is the json with video id: " + obj.playVideoId + " \n";
+        if (obj.playVideoId == youtubeVideoId) {
             document.getElementById('output').innerHTML += "Nothing to do as the video playing is the same than video received\n";
         }
         else {
-            youtubeVideoId = obj.videoId;
+            youtubeVideoId = obj.playVideoId;
             changeVideo(youtubeVideoId);
         }
     }
     else if (e.data.includes('playerState')) {
+        // Handles changes in player state
         obj = JSON.parse(e.data);
         if (obj.playerState == YT.PlayerState.PLAYING) {
             player.playVideo();
@@ -38,6 +41,19 @@ socket.onmessage = function (e) {
         if (obj.playerState == YT.PlayerState.PAUSED) {
             player.pauseVideo();
         }
+        if (obj.playerState == YT.PlayerState.ENDED) {
+            if (nextVideos.length >= 1) {
+                changeVideo(nextVideos.shift())
+            }
+            else {
+                // Stay ended I guess
+            }
+        }
+    }
+    else if (e.data.includes('cueVideoId')) {
+        obj = JSON.parse(e.data);
+        document.getElementById('output').innerHTML += "Cue req with id: " + obj.cueVideoId + " \n";
+        nextVideos.push(obj.cueVideoId);
     }
 
     document.getElementById('output').innerHTML += "<p>Server: " + e.data + "</p>";
@@ -52,6 +68,15 @@ function requestMaster() {
     socket.send('masterRequest')
 }
 
+function sendNewVideoId(id) {
+    var videoIdSocketMsg = `{"playVideoId": "${id}"}`;
+    socket.send(videoIdSocketMsg);
+}
+
+function cueNewVideoId(id) {
+    var videoIdSocketMsg = `{"cueVideoId": "${id}"}`;
+    socket.send(videoIdSocketMsg);
+}
 
 function changeVideo(youtubeVideoId) {
     socket.send('{"videoId": "' + youtubeVideoId + '"}')
@@ -73,8 +98,8 @@ var player;
 var youtubeId;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        height: '195',
-        width: '320',
+        height: '330',
+        width: '550',
         videoId: youtubeVideoId,
         events: {
         'onReady': onPlayerReady,
@@ -107,14 +132,15 @@ function onClientLoad() {
 function onYouTubeApiLoad() {
     gapi.client.setApiKey('AIzaSyCeV93ONxiwmUY7R05wh2ZmDjpHYFw0OAg');
 }
- 
+
 // Called when the search button is clicked in the html code
 function search() {
     var query = document.getElementById('query').value;
     // Use the JavaScript client library to create a search.list() API call.
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
-        q:query
+        q:query,
+        maxResults:4
     });
     // Send the request to the API server, call the onSearchResponse function when the data is returned
     request.execute(onSearchResponse);
@@ -127,8 +153,14 @@ function onSearchResponse(response) {
     console.log(results);
     for(var i = 0; i < results.length; i++) {
         var result = results[i];
-
-        document.getElementById('response').innerHTML += '<p>' + result.id.videoId + ' : ' + result.snippet.title + '</p>';
+        // result.id.videoId
+        document.getElementById('response').innerHTML += `
+        <tr>
+            <td>${result.snippet.title}</td>
+            <td><button onclick="sendNewVideoId('${result.id.videoId}')" class="button is-danger is-small">Play</button></td>
+            <td><button onclick="cueNewVideoId('${result.id.videoId}')" class="button is-info is-small">Cue</button></td>
+        </tr>
+        `;
         
     }
     // document.getElementById('response').innerHTML = responseString;
